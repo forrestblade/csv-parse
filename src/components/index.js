8c576c85b17dcc10ee4c9865d04bb8acb12ebec6
Forrest Blade <forrestblade.code@gmail.com>
2019-07-15 11:40:22 -0500

    style: github ribbon

--- src/components/index.js
+++ src/components/index.js
@@ -26,26 +26,26 @@ export class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-      <div>
-        <h2>CSV to JSON</h2>
-        <CSVReader
-          onFileLoaded={this.handleReadCSV}
-          inputRef={this.fileInput}
-          style={{display: 'none'}}
-          onError={this.handleOnError}
-          configOptions={{header: true}}
-        />
-        <button className='import' onClick={this.handleImportOffer}>Import</button>
-        <br />
-        <textarea value={this.state.data} rows='20' cols='90' readOnly />
-      </div>
-      <hr />
-      <div>
-        <h2>JSON to CSV</h2>
-        <JSONParse />
-      </div>
-      </React.Fragment>
+      <main>
+        <div>
+          <h2>CSV to JSON</h2>
+          <CSVReader
+            onFileLoaded={this.handleReadCSV}
+            inputRef={this.fileInput}
+            style={{display: 'none'}}
+            onError={this.handleOnError}
+            configOptions={{header: true}}
+          />
+          <button className='import' onClick={this.handleImportOffer}>Import</button>
+          <br />
+          <textarea value={this.state.data} rows='20' cols='90' readOnly />
+        </div>
+        <hr />
+        <div>
+          <h2>JSON to CSV</h2>
+          <JSONParse />
+        </div>
+      </main>
     );
   }
 }
